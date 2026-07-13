b7ce6ce5fd2bebd8484d94579788357d9c9fd91d
Igor <igortimao.rezende@gmail.com>
2026-07-12 22:50:57 -0300

    Add files via upload

--- backend/src/server.js
+++ backend/src/server.js
@@ -9,7 +9,12 @@ wss.on("connection", (ws) => {
     ws.on("error", console.error)
 
     ws.on("message", (data) => {
-        wss.clients.forEach((client) => client.send(data.toString()))
+        // não ecoar para o próprio remetente (evita duplicidade no front)
+        wss.clients.forEach((client) => {
+            if (client !== ws && client.readyState === client.OPEN) {
+                client.send(data.toString())
+            }
+        })
     })
 
     console.log("client connected")
